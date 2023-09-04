2a9e32085a18f597bf9a352af9a55f6c7a71cf63
rzhang630 <rzhang630@gatech.edu>
2023-09-04 16:03:03 -0400

    Initial graph commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { DVNetworkSimulator } from './components/DVNetworkSimulator'
+
+function App() {
+  return (
+    <>
+      <DVNetworkSimulator />
+    </>
+  )
+}
+
+export default App
